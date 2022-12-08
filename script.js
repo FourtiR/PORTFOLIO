4b8512075df3c03db7f1c54b682e68a3208464c4
FourtiR <113357324+FourtiR@users.noreply.github.com>
2022-12-08 21:00:30 +0100

    Slider et Flex box

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const mobileNavs = document.getElementsByClassName("nav-links")[0]
-hamburger.addEventListener("click", () => {
-  mobileNavs.classList.toggle("active")
-})
+
+var slide_index = 1;
+slidesDisplay(slide_index);
+function nextSlide(n) {
+slidesDisplay(slide_index += n);
+}
+function currentSlide(n) {
+slidesDisplay(slide_index = n);
+}
+function slidesDisplay(n) {
+var i;
+var slides = document.getElementsByClassName("showSlider");
+if (n > slides.length) { slide_index = 1 }
+if (n < 1) { slide_index = slides.length }
+for (i = 0; i < slides.length; i++) {
+slides[i].style.display = "none";
+}
+slides[slide_index - 1].style.display = "block";
+}
